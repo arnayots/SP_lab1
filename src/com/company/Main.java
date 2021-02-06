1e2bf2377d95ca3545663d53237db46c977ba5f6
arnayots <ar.stoyan@gmail.com>
2021-02-07 00:49:45 +0200

    Release version of app

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,35 +1,55 @@
 package com.company;
 import java.io.*;
+import java.util.Comparator;
+import java.util.TreeSet;
 
 public class Main {
+    public static int MaxWordLenght = 28;
+    public static String FileNameInput = "input.txt";
+    public static String FileNameOut = "out.txt";
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        try {
+            System.out.println("***** Start of program work *****");
+            FileReader fileIn = new FileReader(FileNameInput);
+            TreeSet<String> dictionary = new TreeSet<String>(new Comparator<String>() {
+                @Override
+                public int compare(String o1, String o2) {
+                    if(o1.length() > o2.length())
+                        return 1;
+                    if(o1.length() < o2.length())
+                        return -1;
+                    return o1.compareTo(o2);
+                }
+            });
 
-        try (FileReader reader = new FileReader("test.txt"))
-        {
             int c;
             String word = "";
-            while ((c=reader.read()) != -1)
+            while ((c = fileIn.read()) != -1)
             {
-                int tmp1 = (int) c;
-                if(tmp1 >= 65 && tmp1 <= 90){
+                if((c >= 65 && c <= 90) || (c >= 97 && c <= 122)){
+                    if(c <= 90)
+                        c += 32;
                     word += (char) c;
-                    System.out.println("Symbol " + c + " added to " + word);
                 } else {
-                    //if(word != "")
-                        System.out.println(word);
+                    if(word.length() > MaxWordLenght)
+                        word = word.substring(0, MaxWordLenght);
+                    if(word != "")
+                        dictionary.add(word);
                     word = "";
                 }
             }
+            fileIn.close();
+
+            FileWriter fileOut = new FileWriter(FileNameOut);
+            for(String sample : dictionary){
+                fileOut.write(sample.length() + " " + sample + "\n");
+            }
+            fileOut.close();
+            System.out.println("***** Program finished word succesfully *****");
         }
         catch (IOException ex){
             System.out.println(ex.getMessage());
         }
-
-        System.out.println("End of the program");
-
-
-	// write your code here
     }
 }
