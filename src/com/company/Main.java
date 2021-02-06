6aab21c45d6b2397178a4b525d945a5c98375606
arnayots <ar.stoyan@gmail.com>
2021-02-06 16:44:14 +0200

    Added input & good symbol analysis

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,25 @@
 package com.company;
+import java.io.*;
 
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Hello, world!");
+
+        try (FileReader reader = new FileReader("test.txt"))
+        {
+            int c;
+            while ((c=reader.read()) != -1) {
+                System.out.print((char) c);
+            }
+        }
+        catch (IOException ex){
+            System.out.println(ex.getMessage());
+        }
+
+        System.out.println("End of the program");
+
+
 	// write your code here
     }
 }
